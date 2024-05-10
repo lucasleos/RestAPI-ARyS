d6637e5461ed6a352d32fdebf512c536064ce7fa
lucasleos <lucasleosuarez10@gmail.com>
2024-05-10 19:04:54 -0300

    Fix CSRF

--- src/main/java/com/arys/tp5/productos/SecurityConfig.java
+++ src/main/java/com/arys/tp5/productos/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/").permitAll();
                     auth.anyRequest().authenticated();
